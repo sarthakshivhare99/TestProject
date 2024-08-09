d2b2a7faf6fef8ec90bc41d4758f791eb418ca08
Sarthak Shivhare <83702473+sarthakshivhare99@users.noreply.github.com>
2024-08-09 15:45:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-axios
-      .post("http://localhost:3001/masterApiEndpoint", {
-        plant_id: selectedPlantId,
-        item: 2,
-      })
-      .then((response) => {
-        // Assuming the response contains the data to display in the grid
-        setLoadingData(response.data); // Update this based on your API response
-      })
-      .catch((error) => {
-        console.error("Error fetching data for selected plant:", error);
-      });
+ const columnsVendor = [
+    { field: "vendor", headerName: "Vendor", width: 150 }, // Only Vendor column
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 150,
+      renderCell: (params) => (
+        <Button onClick={() => handleOpenModal(params.row)}>Edit</Button>
+      ),
+    },
+  ];
