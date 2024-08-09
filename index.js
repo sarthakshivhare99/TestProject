1e4fb207ca54c85b17fe128fb418b7f577685aef
Sarthak Shivhare <83702473+sarthakshivhare99@users.noreply.github.com>
2024-08-09 15:52:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
- const columnsVendor = [
-    { field: "vendor", headerName: "Vendor", width: 150 }, // Only Vendor column
-    {
-      field: "actions",
-      headerName: "Actions",
-      width: 150,
-      renderCell: (params) => (
-        <Button onClick={() => handleOpenModal(params.row)}>Edit</Button>
-      ),
-    },
-  ];
+disableSelectionOnClick
