f7f262800587ad6445a8b75b4e4699a32b7e51f8
Sarthak Shivhare <83702473+sarthakshivhare99@users.noreply.github.com>
2024-08-09 16:19:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-disableSelectionOnClick
+{currentView === "vendor" && (
+            <>
+              <TextField
+                label="Vendor"
+                value={newData.vendor || ""}
+                onChange={(e) =>
+                  setNewData({ ...newData, vendor: e.target.value })
+                }
+                fullWidth
+                margin="normal"
+              />
+            </>
+          )}
