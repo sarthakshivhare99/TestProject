c04bc5864fe863d66526295244ba8d2386e303ae
Sarthak Shivhare <83702473+sarthakshivhare99@users.noreply.github.com>
2024-08-13 15:45:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
- {/* Show Add button only if a vendor is selected in the Vehicle view */}
-            {currentView === "vehicle" && selectedVendor && (
-              <Button onClick={() => handleOpenModal()}>Add</Button>
-            )}
-            {/* Show Add button only if a plant is selected in the Section view */}
-            {currentView === "section" && selectedPlant && (
-              <Button onClick={() => handleOpenModal()}>Add</Button>
-            )}
-            {/* Always show Add button for other views */}
-            {(currentView === "plant" || currentView === "loading" || currentView === "scrap" || currentView === "vendor") && (
-              <Button onClick={() => handleOpenModal()}>Add</Button>
-            )}
+ componentsProps={{
+            footer: {
+              style: {
+                margin: 0, 
+                fontSize: '14px'  // Adjust as needed
+              }
+            }
+          }}
+        />
+      )}
