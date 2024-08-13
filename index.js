5cd38593fa8410ea851ad921b0072a0f0ad00b81
Sarthak Shivhare <83702473+sarthakshivhare99@users.noreply.github.com>
2024-08-13 16:16:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-.custom-data-grid-footer .MuiDataGrid-footerContainer {
+/* custom-styles.css */
+.MuiDataGrid-footerContainer .css-16mfp94-MuiTablePagination-root.MuiTablePagination-selectLabel {
   margin: 0 !important;
-  font-size: 14px !important; /* Adjust font size as needed */
 }
