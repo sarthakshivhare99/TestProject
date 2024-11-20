fa98667ee3677d65f1830fec6fe293d183e2669a
Sarthak Shivhare <83702473+sarthakshivhare99@users.noreply.github.com>
2024-11-20 15:55:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-<Grid container spacing={3}>
-                {data && data.map((item, index) => (
-                    <Grid item xs={12} key={index}>
-                        <Paper sx={{ padding: 2, backgroundColor: '#f5f5f5' }}>
-                            <Typography variant="h6">Record {index + 1}</Typography>
-                            <Grid container spacing={2}>
-                                {/* Loop through each key-value pair in the current record */}
-                                {Object.keys(item).map((key) => (
-                                    <React.Fragment key={key}>
-                                        <Grid item xs={6}>
-                                            <Typography variant="body1" fontWeight="bold">
-                                                {key}:
-                                         </Typography>
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <Typography variant="body2">
-                                                {item[key]}
-                                            </Typography>
-                                        </Grid>
-                                    </React.Fragment>
-                                ))}
-                            </Grid>
-                        </Paper>
-                    </Grid>
-                ))}
-            </Grid>
+const convertCamelCaseToReadable = (key) => {
+  return key.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase());
+};
